7289d2a486a2074a879772e03403b126fe7afd20
brunoferreiram <brunoferreiram@outlook.com>
2024-07-11 01:55:31 -0300

    update files

--- iac-cross/variables.tf
+++ iac-cross/variables.tf
@@ -1,7 +1,7 @@
-variable "prefix" {}
-variable "vpc_cidr_block" {}
-variable "cluster_name" {}
-variable "retention_days" {}
-variable "desired_size" {}
-variable "max_size" {}
-variable "min_size" {}
+variable "prefix" {default = "nlw"}
+variable "vpc_cidr_block" {default = "10.0.0.0/16"}
+variable "cluster_name" {default = "nlw-devops"}
+variable "retention_days" {default = "5"}
+variable "desired_size" {default = "1"}
+variable "max_size" {default = "2"}
+variable "min_size" {default = "1"}
